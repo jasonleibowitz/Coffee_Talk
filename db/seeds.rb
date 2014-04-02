afaef04b52142396bb18ae0cb419f1be77436b55
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-04-01 23:15:49 -0400

    Updated CSS and Admin in Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(first_name: "Jason", last_name: "Leibowitz", email: "jasonaleibowitz@gmail.com", dob: "1987-02-26", zipcode: "11201", admin: true, password: "sertgh67", password_confirmation: "sertgh67", profile_pic: "http://i.imgur.com/5z5NbX5.jpg", about: "I'm an aspiring web developer, Apple Geek, New Yorker, Jew & big soccer fan.")
