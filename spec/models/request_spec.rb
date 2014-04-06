b9f1de5f452dc699a960060d3c0f3020daecbb8a
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-04-06 16:21:08 -0400

    method #schedule_meetups passes

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative '../spec_helper.rb'
 
 describe Request do
@@ -16,7 +17,7 @@ describe Request do
     @request.save
     @request2 = Request.new(user_id: @josh.id, confirmed: false, date: '2014-04-05', time: '16')
     @request2.save
-    @request3 = Request.new(user_id: '@bill.id', confirmed: false, date: '2014-04-05', time: '16')
+    @request3 = Request.new(user_id: @bill.id, confirmed: false, date: '2014-04-05', time: '16')
     @request3.save
     @jason.interests << @football
     @jason.interests << @politics
@@ -35,6 +36,15 @@ describe Request do
   describe '#recommendation' do
     it "compares all unconfirmed requests and returns an array of similar users who can meet with owner of request" do
       expect(@request.recommendation).to eq [@request2, @request3]
+    end
+  end
+
+  describe "#schedule_meetups" do
+    it "creates meetings from all matched requests so that all meetings have 3 people" do
+      @request.schedule_meetups
+      expect(@request.confirmed).to eql true
+      expect(@request.meeting_id).not_to be_nil
+      expect(@request.date).not_to be_nil
     end
   end
 
