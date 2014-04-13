a85ba6b89a97e7a3def998fa3ebd1ce83aa0717c
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-04-13 14:22:39 -0400

    fixed error where request_mailer would send before meeting was fully set up, causing errors

--- app/models/request.rb
+++ app/models/request.rb
@@ -100,23 +100,25 @@ class Request < ActiveRecord::Base
         # schedule meetup
         recommended_user_requests = self.recommendation
         meetup = Meeting.create(date: self.date, time: self.time)
+
         self.update(meeting_id: meetup.id)
-        RequestMailer.meeting_scheduled_email(self).deliver
+        self.update(confirmed: true)
 
         recommended_user_requests.first.update(meeting_id: meetup.id)
-        self.update(confirmed: true)
         recommended_user_requests.first.update(confirmed: true)
-        RequestMailer.meeting_scheduled_email(recommended_user_requests.first).deliver
 
 
         recommended_user_requests[1].update(meeting_id: meetup.id)
         recommended_user_requests[1].update(confirmed: true)
-        RequestMailer.meeting_scheduled_email(recommended_user_requests[1]).deliver
-
 
         coffeeshop_recommendation = self.location_recommendation(self.user)
 
         meetup.update(location: coffeeshop_recommendation.first)
+
+        RequestMailer.meeting_scheduled_email(self).deliver
+        RequestMailer.meeting_scheduled_email(recommended_user_requests.first).deliver
+        RequestMailer.meeting_scheduled_email(recommended_user_requests[1]).deliver
+
       end
   end
 
