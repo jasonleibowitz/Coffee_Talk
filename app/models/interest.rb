dfb79de5841f04f858aabe0e9aa9dcb7610d12de
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-04-13 15:33:00 -0400

    not using this model currently, but set up shoulda matcher validations that are commented out in case i use it in the future

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,5 +1,5 @@
 class Interest < ActiveRecord::Base
-  has_and_belongs_to_many(:users)
+  # has_and_belongs_to_many(:users)
 
   validates :name, :description, presence: true
 
---
+++ spec/models/interest_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../spec_helper.rb'
+
+# describe Location do
+
+#   it { should have_and_belong_to_many :users }
+
+#   it { should validate_presence_of :name }
+
+# end
