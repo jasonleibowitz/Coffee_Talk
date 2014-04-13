20f951f119da3f02492d57f86819992a0e4fb4db
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-04-13 14:06:17 -0400

    Changed API urls to constants

--- app/models/foursquare.rb
+++ app/models/foursquare.rb
@@ -1,16 +1,16 @@
 class Foursquare < ActiveRecord::Base
 
-  @@foursquare_url = 'https://api.foursquare.com/v2/venues/explore?client_id=QAOJQNY2JJHJC2DNYHLDH0EEBPFDZAXEOA44DY1X1BZJOJOD&client_secret=4IBKRTTJCKADRNBP3GMLCOYJUF3N21G2HX1VEIE0C4E5D5PX&v='
+  FOURSQUARE_URL = 'https://api.foursquare.com/v2/venues/explore?client_id=QAOJQNY2JJHJC2DNYHLDH0EEBPFDZAXEOA44DY1X1BZJOJOD&client_secret=4IBKRTTJCKADRNBP3GMLCOYJUF3N21G2HX1VEIE0C4E5D5PX&v='
 
-  @@foursquare_venue1 = 'https://api.foursquare.com/v2/venues/'
-  @@foursquare_venue2 = '?client_id=QAOJQNY2JJHJC2DNYHLDH0EEBPFDZAXEOA44DY1X1BZJOJOD&client_secret=4IBKRTTJCKADRNBP3GMLCOYJUF3N21G2HX1VEIE0C4E5D5PX&v=20140331'
+  FOURSQUARE_VENUE1 = 'https://api.foursquare.com/v2/venues/'
+  FOURSQUARE_VENUE2 = '?client_id=QAOJQNY2JJHJC2DNYHLDH0EEBPFDZAXEOA44DY1X1BZJOJOD&client_secret=4IBKRTTJCKADRNBP3GMLCOYJUF3N21G2HX1VEIE0C4E5D5PX&v=20140331'
 
-  @@google_maps = 'http://maps.googleapis.com/maps/api/staticmap?center='
+  GOOGLE_MAPS = 'http://maps.googleapis.com/maps/api/staticmap?center='
 
   @@blacklist = ['starbucks']
 
   def self.foursquare_search(user)
-    foursquare_path = "#{@@foursquare_url}#{Date.current.year}#{Date.current.month}#{Date.current.day}&ll=#{user.latlon.join(",")}&query=coffee"
+    foursquare_path = "#{FOURSQUARE_URL}#{Date.current.year}#{Date.current.month}#{Date.current.day}&ll=#{user.latlon.join(",")}&query=coffee"
     response = HTTParty.get(foursquare_path)
     return response["response"]["groups"].first["items"]
   end
@@ -30,13 +30,13 @@ class Foursquare < ActiveRecord::Base
   end
 
   def self.coffeeshop_venue(venue_id)
-    response = HTTParty.get("#{@@foursquare_venue1}#{venue_id}#{@@foursquare_venue2}")
+    response = HTTParty.get("#{FOURSQUARE_VENUE1}#{venue_id}#{FOURSQUARE_VENUE2}")
     venue = response["response"]["venue"]
     return venue
   end
 
   def self.google_map(lat, lon)
-    @map_url = "#{@@google_maps}#{lat},#{lon}&zoom=17&size=400x400&markers=color:red%7C#{lat},#{lon}&sensor=false"
+    @map_url = "#{GOOGLE_MAPS}#{lat},#{lon}&zoom=17&size=400x400&markers=color:red%7C#{lat},#{lon}&sensor=false"
     return @map_url
   end
 
